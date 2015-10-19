28ad8317a5a640c3a17a507a3952bfdc5076dfc1
andychong1996 <andygg1996personal@gmail.com>
2015-10-19 15:39:30 +0800

    add msg to be prompt to user if seeding successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ require_relative "../app/models/todo.rb"
 class Seeds
 	def self.seeding
 		10.times do
-			Todo.add_for_seed(Faker::Lorem.sentence)
-						
+			Todo.add_for_seed(Faker::Lorem.sentence)						
 		end
+		puts "Seeding completed"
 	end
 
 end
